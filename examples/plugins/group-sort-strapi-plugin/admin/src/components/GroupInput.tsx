cb200e5fd20fcbb6b6fcfe6df18b9f89f6aa47fb
Barkov Daniil <giollord@gmail.com>
2024-12-19 02:42:52 +0200

    Fixes to show input fields

--- examples/plugins/group-sort-strapi-plugin/admin/src/components/GroupInput.tsx
+++ examples/plugins/group-sort-strapi-plugin/admin/src/components/GroupInput.tsx
@@ -1,27 +1,33 @@
-import { TextInput } from '@strapi/design-system';
+import { Field, Flex, TextInput } from '@strapi/design-system';
 import { ChangeEvent, EventHandler, forwardRef } from 'react';
-import { useIntl } from 'react-intl';
+import useTranslation from '../utils/useTranslation';
 
 const GroupInput = forwardRef<HTMLInputElement, any>((props, ref) => {
-  const { attribute, disabled, intlLabel, name, onChange, required, value } = props;
-  const { formatMessage } = useIntl();
+  const { attribute, hint, disabled = false, labelAction, label, name, required = false, onChange, value, error, placeholder } = props;
+  const { formatMessage } = useTranslation();
 
   const handleChange: EventHandler<ChangeEvent<HTMLInputElement>> = (e) => {
     onChange({
-      target: { name, type: attribute.type, value: e.target.value },
+      target: { name, type: attribute.type, value: e.currentTarget.value },
     });
   };
 
   return (
-    <TextInput 
-      ref={ref}
-      name={name}
-      disabled={disabled}
-      value={value}
-      required={required}
-      placeholder={formatMessage(intlLabel)}
-      onChange={handleChange}
-    />
+    <Field.Root name={name} id={name} error={error} hint={hint} required={required}>
+      <Flex direction="column" alignItems="stretch" gap={1}>
+        <Field.Label action={labelAction}>{label}</Field.Label>
+        <TextInput
+          ref={ref}
+          name={name}
+          disabled={disabled}
+          value={value}
+          required={required}
+          placeholder={placeholder || formatMessage({ id: 'group.input.placeholder' })?.toString()}
+          onChange={handleChange} />
+        <Field.Hint />
+        <Field.Error />
+      </Flex>
+    </Field.Root>
   );
 });
 
